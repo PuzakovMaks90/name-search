7eabde7329583234ed308d084abf48215d9981c3
Puzakov Maxym <m.puzakov90@gmail.com>
2022-09-20 23:41:10 +0300

    add count

--- js/main.js
+++ js/main.js
@@ -1,62 +1,23 @@
-// //              0123
-// let userName = 'Dima';
-// let userFamily = 'Stetsenko'
-// // let str1 = 'My name is ' + userName + ' ' + userFamily;
+let str = 'Max Dima Kolya Lena Dima Max Tanya Vova Igor Max Vova Dima';
+let count = 0;
+let subStr = (prompt('Введіть ім\'я для пошуку: Max Dima Kolya Lena Tanya Vova Igor'));
+let pos = str.toLowerCase().indexOf( subStr.toLowerCase() );
 
-// let str2 = `My name is: \n${userName} ${userFamily}`;
-// console.log(str2);
-
-// // prompt('1. Снять;\n2. Внести;\n3. Баланс;\n4. Выход');
-
-// console.log('I\'m a coder');
-
-// console.log(str2.length);
-
-// for (let i = 0; i < str2.length; i++) {
-//     console.log(str2[i]);
-// }
-
-// let str = 'poltava';
-// firstUp();
-// function firstUp() {
-//     let strRes = '';
-//     for (let i = 0; i < str.length; i++) {
-//         if (i == 0) {
-//             strRes = str[i].toUpperCase(); // toLowerCase()
-//         } else {
-//             strRes += str[i];
-//         }
-//     }
-//     console.log(strRes);
-// }
-
-// let str = 'I live in Poltava';
-// //         I*l*v* *n*P*l*a*a
-// replaceStr();
-
-// function replaceStr() {
-//     let strRes = '';
-//     for (let i = 0; i < str.length; i++) {
-//         if ((i + 1) % 2 == 0) {
-//             strRes += '*';
-//             continue;
-//         }
-
-//         strRes += str[i];
-//     }
-    
-//     console.log(strRes);
-// }
-
-let str = 'Dima Kolya Dima Tanya Vova Igor Dima';
-let subStr = 'Dima';
-
-console.log(str, subStr);
-
-// console.log( str.toLowerCase().indexOf( subStr.toLowerCase() ) );
-console.log(str.indexOf(subStr, 12));
-
+while (pos !== -1) {
+    count++;
+    pos = str.toLowerCase().indexOf(subStr.toLowerCase(), pos + 1);
+    if (count >= 1) {
+        countSubStr();
+    } else {
+        alert ('Не знайдено збігів !');
+    }
+}
 
 function countSubStr() {
-    
+
+    if (count >= 1) {
+        alert ('Знайдено збігів: ' + count);
+    }
 }
+
+console.log(count);
